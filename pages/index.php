e0430dad0ba07c9e3fb24af5603597461f7b4888
phamtrongthinh <[200~>
2024-09-03 16:51:37 +0700

    xay dung khung web

---
+++ pages/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type ="text/css" href="../css/style.css">
+<!--khi gan link phai co type ="text/css"-->
+    <title>Web phụ kiện điện thoại</title>
+</head>
+<body>
+    
+    <div class= "wraper">
+        <div class="header">
+            <p> this is header</p>
+        </div>
+        <div class="menu">
+            <p>menu</p>
+        </div>
+        <div id ="main">
+            <div class= "sidebar"></div>
+        </div>
+        <div class="footer">
+            <p>footer</p>
+        </div>
+    </div>
+</body>
+</html>
